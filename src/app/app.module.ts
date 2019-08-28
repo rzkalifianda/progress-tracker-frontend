4e3b458cdacb6c7814bdde37c628d416c8c65a98
rzkalifianda <50090561+rzkalifianda@users.noreply.github.com>
2019-08-28 23:36:37 +0700

    [Core UI] text input (#10)
    * [Core UI] text input
    
    * [Core UI] text input
    
    * [Core UI] text input
    
    * edit border merah
    
    * apus yg dobel

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CustomTextViewComponent } from './core-ui/custom-text-view/custom-text-view.component';
+import { TextInputComponent } from './core-ui/text-input/text-input.component'; 
 
 @NgModule({
   declarations: [
     AppComponent,
-    CustomTextViewComponent
+    CustomTextViewComponent,
+    TextInputComponent
   ],
   imports: [
     BrowserModule,
