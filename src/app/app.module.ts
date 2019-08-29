e3de222bf3a6daac781bd66f792fc8192af30225
FebryFernando21 <51896949+FebryFernando21@users.noreply.github.com>
2019-08-29 23:25:39 +0700

    Core UI Button (#7)
    * login kelar alhamdulilah
    
    * jadi walaikumsalam
    
    * CoreuiButton
    
    * Core Ui Button Update 27Aug19
    
    * revisi button lower case

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,15 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CustomTextViewComponent } from './core-ui/custom-text-view/custom-text-view.component';
-import { TextInputComponent } from './core-ui/text-input/text-input.component'; 
+import { TextInputComponent } from './core-ui/text-input/text-input.component';
+import { CustomButtonComponent } from './core-ui/custom-button/custom-button.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     CustomTextViewComponent,
-    TextInputComponent
+    TextInputComponent,
+    CustomButtonComponent
   ],
   imports: [
     BrowserModule,
