8a3dd187afe8a305101dd2eedf408805b66543e2
antoniusfilian <45317076+antoniusfilian@users.noreply.github.com>
2019-10-01 13:27:10 +0700

    [Admin Scene] Routing admin (#39)
    * app-custom-text-view update
    
    * update routing admin

--- src/app/pages/admin/login-admin/login-admin.component.ts
+++ src/app/pages/admin/login-admin/login-admin.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login-admin',
@@ -7,9 +8,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginAdminComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private router: Router,
+  ) { }
 
   ngOnInit() {
   }
 
+  login(){
+    this.router.navigateByUrl('/home-admin');
+  }
 }
